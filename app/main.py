38b378811da64492761c1567e96da9f1866ae859
peterram-iron <pedro.carneiro@ironhack.com>
2020-09-05 00:05:45 +0100

    lets get this party started

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import pickle
+
+
+app = Flask(__name__)
+
+# any app has routes
+
+# define our first route
+
+#we define it with a python decorator -> we have a door called Felipython
+@app.route('/', methods = ['GET'])
+# whenever this route gets called, it will execute a function
+def index():
+	return "another one bites the dust"
+
+# finally lets run our app
+
+
+if __name__ == '__main__':
+
+	
+
+# all the code that is below here, will only be run when the script is run
+	app.run(host='0.0.0.0', port=8000)
