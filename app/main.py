ceca89785617f13f958e0a1fa0948f08218223c3
peterram-iron <pedro.carneiro@ironhack.com>
2020-09-05 00:54:35 +0100

    trying again

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET'])
 # whenever this route gets called, it will execute a function
 def index():
-	return "Maximus: My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions, loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, in this life or the next"
+	return "Maximus: My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions, loyal servant to the true emperor, Marcus Aurelius. Father to a murdered son, husband to a murdered wife. And I will have my vengeance, in this life or the next! Maximus Maximum"
 
 # finally lets run our app
 
